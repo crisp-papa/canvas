96f93c3feedf1c45e74fc0becae2a31f7ca44e6f
floatingeye <eriklougee@gmail.com>
2024-05-20 18:08:04 -0400

    Add basic keyhandler concept - Removal of some html elements that are no longer used - Removal of css rules

--- javascript/main.js
+++ javascript/main.js
@@ -2,82 +2,29 @@ import { getRandomRGB } from './utility.js';
 
 const canvas = document.getElementById('main');
 const context = canvas.getContext('2d');
-const consoleDisplay = document.getElementById('console');
-const instructions = document.getElementById('information');
-let drawing = false;
+const output = document.getElementById('output');
 
 function handleMouseDown(event) { 
   if (event.button == 0) { 
-    drawing = true;
-    context.beginPath();
-  }
-
-  if (event.button === 2) { 
-    context.fillStyle = "white";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    // Left click
   }
 }
 
 function handleMouseMovement(event) {
-  if (drawing) {
-    context.lineTo(event.offsetX, event.offsetY);
-    context.stroke();
-  }
-
-  consoleDisplay.innerText = `Mouse position X: ${event.offsetX}, Mouse position Y: ${event.offsetY}`;
+  output.innerText = `Mouse position X: ${event.offsetX}, Mouse position Y: ${event.offsetY}`;
 }
 
 function handleMouseUp(event) {
-  drawing = false;
+  // Unclick
 }
 
 function initialize() {
   if (canvas.getContext) {
     // Resize the canvas based on the size of the body element and the default html margins
     initializeCanvas();
-
-    // Draw button event listener
-    document.getElementById('draw').addEventListener('click', initializeDrawButton);
-
   } else {
     // canvas-unsupported code here
   }
-}
-
-function initializeDrawButton() {
-  // Event listeners for mouse movement and clicks on the canvas
-  canvas.addEventListener('mousemove', handleMouseMovement);
-  canvas.addEventListener('mousedown', handleMouseDown);
-  canvas.addEventListener('mouseup', handleMouseUp);
-
-  canvas.addEventListener('contextmenu', (event) => {
-      event.preventDefault(); // Prevent the default context menu from appearing
-  });
-
-  instructions.innerText = 'Left click to draw on the canvas.\nRight click to clear the canvas.';
-
-  hideButtons();
-
-  // Display functions related to drawing
-  const functions = document.getElementById('draw-functions')
-  functions.classList.toggle('hidden');
-  functions.querySelector('#line-color').addEventListener('input', (event) => {
-    context.strokeStyle = event.target.value;
-  });
-
-  functions.querySelector('#line-width').addEventListener('input', (event) => {
-    context.lineWidth = event.target.value;
-  });
-
-  functions.querySelector('#background-color').addEventListener('input', (event) => { 
-    context.fillStyle = event.target.value;
-    context.fillRect(0, 0, canvas.width, canvas.height);
-  });
-  context.lineCap = 'round';
-}
-
-function hideButtons() {
-  document.getElementById('draw').classList.toggle('hidden');
 }
 
 function initializeCanvas() { 
@@ -93,6 +40,19 @@ function initializeCanvas() {
   avatar.onload = () => {
     context.drawImage(avatar, 16, 0, 16, 24, 0, 0, 16, 24);
   }
+
+  // Key handler
+  document.addEventListener('keydown', (event) => {
+      switch (event.code) {
+        default:
+          console.log(`Key Down: ${event.key} (code: ${event.code})`);
+          break;
+      }
+  });
+
+  canvas.addEventListener('mousemove', handleMouseMovement);
+  canvas.addEventListener('mousedown', handleMouseDown);
+  canvas.addEventListener('mouseup', handleMouseUp);
 }
 
 window.addEventListener('load', initialize);
