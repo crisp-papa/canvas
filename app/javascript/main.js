b41411dd78200257cca7917bd7333da4fe1abc24
floatingeye <eriklougee@gmail.com>
2024-05-21 11:21:44 -0400

    Adding a hash map for the values that correspond to the pixels in the sprite sheet

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,4 +1,4 @@
-import { getRandomRGB } from './utility.js';
+import { getRandomRGB, getAvatarSourceImageValues } from './utility.js';
 
 const canvas = document.getElementById('main');
 const context = canvas.getContext('2d');
@@ -6,7 +6,7 @@ const output = document.getElementById('output');
 
 function handleMouseDown(event) { 
   if (event.button == 0) { 
-    // Left click
+    // Left click (move / attack / pick up item / use door / open chest / etc)
   }
 }
 
@@ -29,16 +29,23 @@ function initialize() {
 
 function initializeCanvas() { 
   const display = window.getComputedStyle(document.querySelector('.display'));
-  canvas.setAttribute('width', display.getPropertyValue('width'));
-  canvas.setAttribute('height', window.innerHeight - 24);
 
-  context.fillStyle = "#0f0f0f"; 
+  canvas.setAttribute('width', display.getPropertyValue('width'));
+  console.log(`Setting width to ${display.getPropertyValue('width')}`);
+
+  canvas.setAttribute('height', window.innerHeight - 24);
+  console.log(`Setting height to ${window.innerHeight - 24}`);
+
+  context.fillStyle = "#212121"; 
   context.fillRect(0, 0, canvas.width, canvas.height);
   
   let avatar = new Image();
   avatar.src = 'assets/oryx/Avatar.png';
   avatar.onload = () => {
-    context.drawImage(avatar, 16, 0, 16, 24, 0, 0, 16, 24);
+    const [sx, sy, sw, sh] = getAvatarSourceImageValues('right');
+    const [dx, dy, dw, dh] = [0, 0, 16, 24];
+    // image, sx, sy, sw, sh, dx, dy, dw, dh
+    context.drawImage(avatar, sx, sy, sw, sh, dx, dy, dw, dh);
   }
 
   // Key handler
--- app/javascript/utility.js
+++ app/javascript/utility.js
@@ -12,3 +12,33 @@ export function getRandomNumber(min = 0, max) {
 export function getRandomRGB() { 
   return `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)})`;
 }
+
+
+export function getAvatarSourceImageValues(orientation = 'left') {
+  const map = new Map([
+    // sx, sy, sw, sh
+    ['shadow', [0, 0, 16, 24]],
+    ['left', [16, 0, 16, 24]],
+    ['leftMelee', [32, 0, 16, 24]],
+    ['leftUnknown', [48, 0, 16, 24]], // Honestly not sure what this sprite is supposed to represent
+    ['leftMeleeLarge', [64, 0, 16, 24]],
+    ['leftRanged', [80, 0, 16, 24]],
+    ['leftCrouched', [16, 24, 16, 24]],
+    ['leftMeleeCrouched', [32, 24, 16, 24]],
+    ['leftUnknownCrouched', [48, 24, 16, 24]],
+    ['leftMeleeLargeCrouched', [64, 24, 16, 24]],
+    ['leftRangedCrouched', [80, 24, 16, 24]],
+    ['right', [16, 48, 16, 24]],
+    ['rightMelee', [32, 48, 16, 24]],
+    ['rightUnknown', [48, 48, 16, 24]], // Same
+    ['rightMeleeLarge', [64, 48, 16, 24]],
+    ['rightRanged', [80, 48, 16, 24]],
+    ['rightCrouched', [16, 72, 16, 24]],
+    ['rightMeleeCrouched', [32, 72, 16, 24]],
+    ['rightUnknownCrouched', [48, 72, 16, 24]],
+    ['rightMeleeLargeCrouched', [64, 72, 16, 24]],
+    ['rightRangedCrouched', [80, 72, 16, 24]],
+  ]);
+
+  return map.get(orientation);
+}
